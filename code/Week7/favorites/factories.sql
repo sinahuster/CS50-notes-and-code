c0e6d41918b3e51143227443455c4e22e9aaf825
Sina Huster <sina@huster.me.uk>
2025-08-22 15:27:29 +0200

    Create factories.sql

---
+++ code/Week7/favorites/factories.sql
@@ -0,0 +1,17 @@
+UPDATE shows SET title = 'The Office' WHERE title LIKE '%office%';
+
+UPDATE shows SET title = 'Gilmore Girls' WHERE title LIKE '%gilmore%';
+
+UPDATE shows SET title = 'Avatar: The Last Airbender' WHERE title LIKE '%avatar%';
+
+UPDATE shows SET title = 'Brooklyn Nine-Nine' WHERE title LIKE '%brooklyn%';
+
+UPDATE shows SET title = 'Family Guy' WHERE title LIKE '%family%';
+
+UPDATE shows SET title = 'Billions' WHERE title LIKE '%billions%';
+
+UPDATE shows SET title = 'Anne with an E' WHERE title LIKE '%anne%';
+
+UPDATE shows SET title = 'Sherlock Holmes' WHERE title LIKE '%sherlock%';
+
+UPDATE shows SET title = 'Criminal Minds' WHERE title LIKE '%criminal%';
